79453653ab310b8ac60ddb4ebfc3d1b696191a9b
Alexander Potapov <aleksandr.potapov@videobet.com>
2012-08-22 12:56:03 +0300

    initial commit

---
+++ bundleA/src/a/b/e/Activator.java
@@ -0,0 +1,14 @@
+package a.b.e;
+
+import a.b.c.Class1;
+import a.b.c.Class2;
+import a.b.d.Class3;
+
+public class Activator {
+
+	public void test() {
+		Class1 c1 = new Class1();
+		Class2 c2 = new Class2();
+		Class3 c3 = new Class3();
+	}
+}
---
+++ projectB/src/main/java/a/b/d/Class3.java
@@ -0,0 +1,5 @@
+package a.b.d;
+
+public class Class3 {
+
+}
